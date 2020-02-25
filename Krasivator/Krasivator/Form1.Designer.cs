08fe3a1ddc598d7cad2c3491f7a5b8a4c7ea736a
leeroyka <blokhin321@gmail.com>
2020-02-26 02:18:38 +0300

    processing

--- Krasivator/Krasivator/Form1.Designer.cs
+++ Krasivator/Krasivator/Form1.Designer.cs
@@ -40,9 +40,22 @@
             this.EditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.xToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelRight = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelRightInst = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnRightInst1 = new System.Windows.Forms.Button();
+            this.btnRightInst2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelRightConfirm = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnConfirm = new System.Windows.Forms.Button();
             this.panelLeft.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelRight.SuspendLayout();
+            this.panelRightInst.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panelRightConfirm.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelLeft
@@ -68,6 +81,7 @@
             this.btnLeftInst.TabIndex = 0;
             this.btnLeftInst.Text = "И";
             this.btnLeftInst.UseVisualStyleBackColor = true;
+            this.btnLeftInst.Click += new System.EventHandler(this.btnLeftInst_Click);
             // 
             // btnLeftEff
             // 
@@ -79,6 +93,7 @@
             this.btnLeftEff.TabIndex = 1;
             this.btnLeftEff.Text = "Э";
             this.btnLeftEff.UseVisualStyleBackColor = true;
+            this.btnLeftEff.Click += new System.EventHandler(this.btnLeftEff_Click);
             // 
             // menuStrip1
             // 
@@ -119,13 +134,14 @@
             // открытьToolStripMenuItem
             // 
             this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
             // 
             // сохранитьToolStripMenuItem
             // 
             this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.сохранитьToolStripMenuItem.Text = "Сохранить";
             // 
             // выйтиToolStripMenuItem
@@ -153,21 +169,119 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(62, 27);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.Size = new System.Drawing.Size(687, 588);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panelRight
+            // 
+            this.panelRight.AutoSize = true;
+            this.panelRight.Controls.Add(this.panelRightInst);
+            this.panelRight.Controls.Add(this.panelRightConfirm);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panelRight.Location = new System.Drawing.Point(752, 24);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(206, 591);
+            this.panelRight.TabIndex = 3;
+            this.panelRight.WrapContents = false;
+            // 
+            // panelRightInst
+            // 
+            this.panelRightInst.BackColor = System.Drawing.Color.DimGray;
+            this.panelRightInst.Controls.Add(this.btnRightInst1);
+            this.panelRightInst.Controls.Add(this.btnRightInst2);
+            this.panelRightInst.Location = new System.Drawing.Point(3, 3);
+            this.panelRightInst.Name = "panelRightInst";
+            this.panelRightInst.Size = new System.Drawing.Size(200, 100);
+            this.panelRightInst.TabIndex = 4;
+            // 
+            // btnRightInst1
+            // 
+            this.btnRightInst1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnRightInst1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRightInst1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRightInst1.ForeColor = System.Drawing.Color.White;
+            this.btnRightInst1.Location = new System.Drawing.Point(3, 3);
+            this.btnRightInst1.Name = "btnRightInst1";
+            this.btnRightInst1.Size = new System.Drawing.Size(30, 30);
+            this.btnRightInst1.TabIndex = 1;
+            this.btnRightInst1.Text = "1";
+            this.btnRightInst1.UseVisualStyleBackColor = true;
+            // 
+            // btnRightInst2
+            // 
+            this.btnRightInst2.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnRightInst2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRightInst2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRightInst2.ForeColor = System.Drawing.Color.White;
+            this.btnRightInst2.Location = new System.Drawing.Point(39, 3);
+            this.btnRightInst2.Name = "btnRightInst2";
+            this.btnRightInst2.Size = new System.Drawing.Size(30, 30);
+            this.btnRightInst2.TabIndex = 2;
+            this.btnRightInst2.Text = "2";
+            this.btnRightInst2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(56, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 591);
+            this.panel1.TabIndex = 5;
+            // 
+            // panelRightConfirm
+            // 
+            this.panelRightConfirm.BackColor = System.Drawing.Color.DimGray;
+            this.panelRightConfirm.Controls.Add(this.btnConfirm);
+            this.panelRightConfirm.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panelRightConfirm.Location = new System.Drawing.Point(3, 109);
+            this.panelRightConfirm.Name = "panelRightConfirm";
+            this.panelRightConfirm.Size = new System.Drawing.Size(200, 37);
+            this.panelRightConfirm.TabIndex = 5;
+            this.panelRightConfirm.WrapContents = false;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirm.Font = new System.Drawing.Font("Candara", 9F);
+            this.btnConfirm.ForeColor = System.Drawing.Color.White;
+            this.btnConfirm.Location = new System.Drawing.Point(3, 3);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(188, 30);
+            this.btnConfirm.TabIndex = 1;
+            this.btnConfirm.Text = "Подтвердить";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 615);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelRight);
             this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -180,6 +294,12 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelRight.ResumeLayout(false);
+            this.panelRightInst.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelRightConfirm.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,6 +319,14 @@
         private System.Windows.Forms.ToolStripMenuItem EditToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem xToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel panelRight;
+        private System.Windows.Forms.FlowLayoutPanel panelRightInst;
+        private System.Windows.Forms.Button btnRightInst1;
+        private System.Windows.Forms.Button btnRightInst2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel panelRightConfirm;
+        private System.Windows.Forms.Button btnConfirm;
     }
 }
 
--- Krasivator/Krasivator/Form1.cs
+++ Krasivator/Krasivator/Form1.cs
@@ -12,9 +12,11 @@ namespace Krasivator
 {
     public partial class Form1 : Form
     {
+        int imageW, imageH;
         public Form1()
         {
             InitializeComponent();
+            setInactiveRight();
             setDefaultColors();
             openFile();
         }
@@ -30,14 +32,19 @@ namespace Krasivator
         MyImage img,img_out;
         void renderImage(string dir)
         {
-                img = new MyImage(dir);
-                var (w, h) = img.getSize();
-                pictureBox1.Image = img.bmp;
+            img = new MyImage(dir);
+            var (w, h) = img.getSize();
+            imageW = w;
+            imageH = h;
+            pictureBox1.Image = img.bmp;
             
         }
         void renderImage(int w,int h)
         {
             img = new MyImage(w, h);
+            imageW = w;
+            imageH = h;
+
             for (int i = 0; i < h; ++i)
             {
                 for (int j = 0; j < w; ++j)
@@ -48,8 +55,15 @@ namespace Krasivator
             pictureBox1.Image = img.bmp;
             
         }
+        void renderSecondImage(string dir)
+        {
+            MyImage simg = new MyImage(dir);
+            pictureBox2.Visible = true;
+            pictureBox2.Image = simg.bmp;
+        }
         void setDefaultColors()
         {
+            
             menuStrip1.BackColor = Color.FromArgb(255,36, 47, 61);
             Color colorText = Color.FromArgb(255, 87, 102, 115);
             EditToolStripMenuItem.ForeColor = Color.White;
@@ -65,13 +79,36 @@ namespace Krasivator
             xToolStripMenuItem.ForeColor = colorText;
             this.BackColor = Color.FromArgb(255, 14, 22, 33);
             panelLeft.BackColor = Color.FromArgb(255, 22, 33, 43);
+            panelRight.BackColor = Color.FromArgb(255, 22, 33, 43);
             btnLeftInst.BackColor = Color.FromArgb(255, 42, 83, 120);
             btnLeftEff.BackColor = Color.FromArgb(255, 42, 83, 120);
+            btnConfirm.BackColor = Color.FromArgb(255, 42, 83, 120);
+            btnRightInst1.BackColor = Color.FromArgb(255, 42, 83, 120);
+            btnRightInst2.BackColor = Color.FromArgb(255, 42, 83, 120);
+            panelRightInst.BackColor = Color.FromArgb(255, 32, 43, 54);
+            panelRightConfirm.BackColor = Color.FromArgb(255, 32, 43, 54);
             btnLeftEff.ForeColor = Color.White;
             btnLeftInst.ForeColor = Color.White;
             btnLeftInst.FlatAppearance.BorderColor = Color.FromArgb(255, 108, 120, 131);
             btnLeftEff.FlatAppearance.BorderColor = Color.FromArgb(255, 108, 120, 131);
-            
+
+            btnLeftInst.Enabled = false;
+            panelRightInst.Visible = true;
+        }
+        void setInactiveRight()
+        {
+            panelRightInst.Visible = false;
+            panelRightConfirm.Visible = false;
+        }
+        void setInactiveLeft()
+        {
+            btnLeftEff.Enabled = false;
+            btnLeftInst.Enabled = false;
+        }
+        void setActiveLeft()
+        {
+            btnLeftEff.Enabled = true;
+            btnLeftInst.Enabled = true;
         }
         private void xToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -147,6 +184,79 @@ namespace Krasivator
         private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        bool isConfirm = false;
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset;
+            int yOffset;
+
+            if (e.Button == MouseButtons.Left && isConfirm)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset = -e.Y - SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset, yOffset);
+                isMouseDown = true;
+            }
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X - this.Location.X - panel1.Location.X, mouseOffset.Y - this.Location.Y- panel1.Location.Y);
+                pictureBox2.Location = mousePos;
+            }
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            isConfirm = false;
+            setInactiveRight();
+            setActiveLeft();
+            btnLeftInst.Enabled = false;
+            panelRightInst.Visible = true;
+        }
+
+        private void btnLeftEff_Click(object sender, EventArgs e)
+        {
+            setInactiveRight();
+            setActiveLeft();
+            btnLeftEff.Enabled = false;
+        }
+
+        private void btnLeftInst_Click(object sender, EventArgs e)
+        {
+            setInactiveRight();
+            panelRightInst.Visible = true;
+            setActiveLeft();
+            btnLeftInst.Enabled = true;
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "Картинки|*.jpg";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string dir = dialog.FileName;
+                    setInactiveRight();
+                    isConfirm = true;
+                    setInactiveLeft();
+                    panelRightConfirm.Visible = true;
+                    renderSecondImage(dir);
+                }
+            }
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
